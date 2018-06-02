0dac6b5c80ee6df6d525aa4d330e04d7860b73a7
Ian Bates <setab.nai@gmail.com>
2018-06-02 23:55:01 +0100

    [Analysis] Remove obsolete ccg grouping routine (measures level).

--- Analysis/cdg_91_qof.R
+++ Analysis/cdg_91_qof.R
@@ -572,57 +572,20 @@ f__91__measures <- function(
     # combine
 
     m.comb <- list(m.ind, m.prev) %>% rbindlist(use.names = TRUE)
-}
 
-#' Calculate ccg groups
-#'
-#' Add ccg groups to the measures.
-#'
-f__91__measures_ccg_group <- function(
-    qof_measures
-    , lu.orgs.ccgs.groups = NA
-) {
-    cat("INFO: f__91__measures_ccg_group: adding ccg groups to measures ...", "\n")
+    # Save
 
-    if (!any(is.na(lu.orgs.ccgs.groups))) {
+    if (bWriteCSV) {
+        cat("INFO: f__91__measures: saving m.comb ...", "\n")
 
-        m.groups <- qof_measures %>%
-            # choose ccg quantities
-            filter(org.type == "ccg") %>%
-            filter(m.stat %in% c("numerator", "denominator")) %>%
-            # spin up on stat
-            dcast(... ~ m.stat, value.var = "value", fun = sum) %>%
-            # tag ccg groups
-            merge(
-                lu.orgs.ccgs.groups %>% select(ccg_code, ccg_group_code, ccg_group_type)
-                , all.y = TRUE
-                , by = "ccg_code"
-            ) %>%
-            mutate(
-                org.type = paste(ccg_group_type, "instance", sep = ", ")
-                , ccg_code = ccg_group_type
-                , practice_code = ccg_group_code
-            ) %>% select(-starts_with("ccg_group")) %>%
-            # summarise over the new ccg groups
-            group_by_at(vars(-numerator, -denominator)) %>%
-            summarise_at(vars(numerator, denominator), sum) %>%
-            ungroup() %>%
-            mutate(value = numerator * 100 / denominator) %>%
-            # spin back down
-            melt(
-                measure.vars = c("numerator", "denominator", "value")
-                , variable.name = "m.stat", variable.factor = FALSE
-            )
-
-        m.comb <- list(qof_measures, m.groups) %>% rbindlist(use.names = TRUE)
+        this.file <- paste0("./Results/", qof_root, "_all", file_suffix, ".csv")
+        fwrite(m.comb, file = this.file)
 
     } else {
-        m.comb <- qof_measures
+        cat("INFO: f__91__measures: NOT saving m.comb ...", "\n")
     }
 
-    # return
-
-    return(m.comb)
+    invisible(m.comb)
 }
 
 #' Merge ccg groups into orgref
