df388f40d005a8122f303ec14abbd91b38a08df7
Ian Bates <ian.bates@nottscc.gov.uk>
2018-10-22 09:12:17 +0100

    [R, man] Documentation updates.
     - data
     - other changes

--- R/data.R
+++ R/data.R
@@ -2,6 +2,31 @@
 # data.R
 #
 
+#' QOF datasets
+#'
+#' @family qof_datasets
+#' @family qof_data
+#' @family qof_meta
+#'
+#' @name qof_datasets
+NULL
+
+#' QOF data
+#'
+#' @family qof_datasets
+#' @family qof_data
+#'
+#' @name qof_data
+NULL
+
+#' QOF meta
+#'
+#' @family qof_datasets
+#' @family qof_meta
+#'
+#' @name qof_meta
+NULL
+
 #' QOF reference data for organisations
 #'
 #' \preformatted{
@@ -23,8 +48,21 @@
 #' }
 #'
 #' @family qof_meta
+#' @name qof_meta_org
 #'
 "qof_1617_meta_org"
+
+#' @name qof_meta_org
+"qof_1516_meta_org"
+
+#' @name qof_meta_org
+"qof_1415_meta_org"
+
+#' @name qof_meta_org
+"qof_1314_meta_org"
+
+#' @name qof_meta_org
+"qof_1213_meta_org"
 
 
 #' QOF reference data for organisations
@@ -40,10 +78,34 @@
 #' $ patient_list_type          : chr
 #' }
 #'
+#' \describe{
+#'   \item{\code{patient_list_type}}{
+#'   One of
+#'   \code{TOTAL}
+#'   , \code{15OV}, \code{16OV}, \code{17OV}, \code{18OV}
+#'   , \code{45OV}, \code{50OV}
+#'   , \code{25_64_F}
+#'   , \code{30_74}
+#'   , \code{54UN_F}
+#'   }
+#' }
+#'
 #' @family qof_meta
+#' @name qof_meta_ind
 #'
 "qof_1617_meta_ind"
 
+#' @name qof_meta_ind
+"qof_1516_meta_ind"
+
+#' @name qof_meta_ind
+"qof_1415_meta_ind"
+
+#' @name qof_meta_ind
+"qof_1314_meta_ind"
+
+#' @name qof_meta_ind
+"qof_1213_meta_ind"
 
 #' QOF data for prevalence
 #'
@@ -56,20 +118,22 @@
 #' }
 #'
 #' @family qof_data
+#' @name qof_data_prev
 #'
 "qof_1617_data_prev"
 
-#' @inherit qof_1617_data_prev
-#'
+#' @name qof_data_prev
 "qof_1516_data_prev"
 
-#' @inherit qof_1617_data_prev
-#'
+#' @name qof_data_prev
 "qof_1415_data_prev"
 
-#' @inherit qof_1617_data_prev
-#'
+#' @name qof_data_prev
 "qof_1314_data_prev"
+
+#' @name qof_data_prev
+"qof_1213_data_prev"
+
 
 #' QOF data for indicators
 #'
@@ -82,11 +146,28 @@
 #'
 #' \describe{
 #'   \item{\code{measure}}{
-#'   One of \code{ACHIEVED_POINTS}, \code{DENOMINATOR}, \code{EXCEPTIONS},
-#'   \code{NUMERATOR}, \code{REGISTER}
+#'   One of
+#'   \code{ACHIEVED_POINTS}
+#'   , \code{DENOMINATOR}
+#'   , \code{EXCEPTIONS}
+#'   , \code{NUMERATOR}
+#'   , \code{REGISTER}
 #'   }
 #' }
 #'
 #' @family qof_data
+#' @name qof_data_ind
 #'
 "qof_1617_data_ind"
+
+#' @name qof_data_ind
+"qof_1516_data_ind"
+
+#' @name qof_data_ind
+"qof_1415_data_ind"
+
+#' @name qof_data_ind
+"qof_1314_data_ind"
+
+#' @name qof_data_ind
+"qof_1213_data_ind"
