baca24a90d9a1df76182cd059c05723795c5e3b2
Ian Bates <ian.bates@nottscc.gov.uk>
2018-10-23 22:52:47 +0100

    [R] Really include the utility routines.
     - develop setnames()

--- R/utils.R
+++ R/utils.R
@@ -75,5 +75,87 @@ sum.rmna <- function(x) {
 #' @family Helper routines
 #'
 setnames.clean <- function(x) {
-    setnames(x, make.names(tolower(colnames(x))))
+    setnames(x, gsub("\\.*$", "", make.names(tolower(colnames(x)))))
+}
+
+#' infix like but ignore case
+#'
+#' @seealso %like%
+#'
+`%ilike%` <- function(vector, pattern) {
+    if (is.factor(vector)) {
+        as.integer(vector) %in% grep(pattern, levels(vector), ignore.case = TRUE)
+    }
+    else {
+        grepl(pattern, vector, ignore.case = TRUE)
+    }
+}
+
+#' My summary
+#'
+#' factor version of summary
+fsummary <- function(x, ...) {
+    x %>%
+        mutate_if(is.character, as.factor) %>%
+        summary(...)
+}
+
+#' My glimpse
+#'
+#' factor version of glimpse
+fglimpse <- function(x, width = NULL, ...) {
+    if (!"data.frame" %in% class(x))
+        stop("need a data frame object")
+
+    if (is.null(width))
+        width = options()$width
+
+    cat("Observations:", nrow(x), "\n")
+    cat("Variables:", ncol(x), "\n")
+
+    these_names <- names(x)
+    n_names_max <- max(nchar(these_names))
+
+    these_names %>%
+        lapply(
+            function(field, x, width) {
+                spad <- paste0(rep(" ", n_names_max - nchar(field) + 1), collapse = "")
+                msg <- paste0("$ ", field, spad)
+                vx <- as.data.frame(x)[, field]
+
+                if (!is.character(vx)) {
+                    msg <- paste0(msg, "<", vx %>% class() %>% substr(., 1, 3), "> ")
+                    s <- summary(vx)
+                    msg <- paste0(
+                        msg
+                        , data.frame(n = names(s), v = formatC(as.numeric(s), digits = 3, width = 0)) %>%
+                            mutate(l = paste0(n, ": ", v)) %>%
+                            .$l %>%
+                            paste(collapse = ", ")
+                    )
+                } else {
+                    f <- vx %>% unique()
+                    nf <- length(f)
+                    msg <- paste0(
+                        msg, "<chr n=", nf, "> "
+                        , ifelse(nf > 0, "\"", "")
+                        , paste(f, collapse = "\", \"")
+                        , ifelse(nf > 0, "\"", "")
+                    )
+                }
+
+                nmsg <- nchar(msg)
+
+                if (!is.null(width))
+                    if (nmsg > width)
+                        msg <- paste0(substr(msg, 1, width - 3), "...")
+
+                cat(msg, "\n")
+
+            }
+            , x = x
+            , width = width
+        )
+
+    invisible(x)
 }
