74c68bc15909b0e57ddd205cf6f2b470cee7035c
Ian Bates <ian.bates@nottscc.gov.uk>
2018-11-11 16:08:33 +0000

    [test] Update tests
     - consider problem cases
     - set up verbosity levels
     - move generic setup variables to helper- files

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,14 @@
 library(testthat)
 library(qofvariation.ncc)
 
+# setup
+previous_verbosity <- verbosity_level
+verbosity.level <- verbosity_levels["suppress"]
+
+proj_get(quiet = TRUE)
+
+
 test_check("qofvariation.ncc")
+
+
+verbosity <- previous_verbosity
--- tests/testthat/test-transform.R
+++ tests/testthat/test-transform.R
@@ -1,40 +1,10 @@
-context("test-transform")
-
-qof_root = c(
-    "qof-1718", "qof-1617"
-    , "qof-1516", "qof-1415", "qof-1314", "qof-1213", "qof-1112"
-    , "qof-1011", "qof-0910", "qof-0809", "qof-0708", "qof-0607"
-    , "qof-0506", "qof-0405"
-)[2]
-
-bExtractFromRaw = FALSE
-bWriteCSV = FALSE
-bSaveData = FALSE
-bLoadData = FALSE
-
 lu_local <- f__transform__create_local_lu()
 
 lu_ccgs <- lu_local$lu_ccgs
 lu_ccg_groups <- lu_local$lu_ccg_groups
 
 
-# find unique values across a number of dataframes
-#
-# function to take a list of dataframes, extract values from common field
-# name, and find unique value
-#
-l__unique <- function(x, method = c("sapply", "bindrows")) {
-    method <- match.arg(method)
-    if (method == "bindrows") {
-        x %>% bind_rows() %>% .$qof_period %>% unique()
-    } else {
-        x %>%
-            lapply(function(y) {
-                y$qof_period %>% unique()}
-            ) %>%
-            unlist() %>% unique()
-    }
-}
+context("test-transform-single-root")
 
 
 test_that("transform works", {
@@ -149,14 +119,8 @@ test_that("transform works", {
 })
 
 
-# multiple roots
 
-qof_roots <- c(
-    "qof-1718", "qof-1617"
-    , "qof-1516", "qof-1415", "qof-1314", "qof-1213", "qof-1112"
-    , "qof-1011", "qof-0910", "qof-0809", "qof-0708", "qof-0607"
-    , "qof-0506", "qof-0405"
-)[c(2, 3)]
+context("test-transform-multiple-roots")
 
 
 test_that("transform works with mulitple qof periods", {
